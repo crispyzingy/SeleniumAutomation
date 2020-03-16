8e91535e514973999b2df0c77c3c91a9dc4c18a7
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-16 23:49:39 +0530

    docs strings updated

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -6,6 +6,6 @@ browser.get("https://automatetheboringstuff.com")
 elem = browser.find_element_by_css_selector(
     ".main > ul:nth-child(16) > li:nth-child(1) > a:nth-child(1)"
 )
-elem.click()
+elem.click()  # send_keys(), submit() for search. Browser - back, forward, refresh, quit
 elems = browser.find_elements_by_css_selector("p")
 print(len(elems))
